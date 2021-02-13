477fcf692a58c974b32b1a3bc71b7ff7ec8934db
Ege Eke <egeeke2001@gmail.com>
2021-02-14 01:31:09 +0300

    Project 3

---
+++ Project3.playground/Contents.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+//Problem 3
+//The prime factors of 13195 are 5, 7, 13 and 29.
+//What is the largest prime factor of the number 600851475143 ?
+//Answer:  6857
+
+let number: Int = 600_851_475_143
+var largestPrimeFactor: Int? = nil
+
+for i in 2...number {
+  if number.isMultiple(of: i) {
+    if isPrimeNumber(i) {
+      largestPrimeFactor = i
+    }
+  }
+}
+
+func isPrimeNumber(_ number: Int) -> Bool {
+  if number == 0 || number == 1 {
+    return false
+  }
+  else {
+    for i in 2..<number {
+      if number % i == 0 {
+        return false
+      }
+    }
+  }
+  return true
+}
+
+largestPrimeFactor
